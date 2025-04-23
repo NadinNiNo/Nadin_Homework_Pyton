9ea53409f5a714fb0d30046e474017037c27442a
NadinNiNo <ilja.egorycheff@gmail.com>
2025-04-24 00:45:59 +0300

    Update lesson_1_task_3.py

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -1,5 +1,5 @@
-first_name = input("Надежда")
+first_name = input("Введите ваше имя")
 
-last_name = input("Егорычева")
+last_name = input("Введите вашу фамилию")
 
 print("Вас зовут : " +  last_name + first_name)
